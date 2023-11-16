e99898694f365ea8cc38a46dc0451921cfaacbf3
ricocatford <s7funoob@gmail.com>
2023-11-16 11:24:02 +0000

    feat: product cards

--- main.js
+++ main.js
@@ -5,21 +5,43 @@ let cart = [];
 
 function displayProducts(products) {
     products.forEach((product) => {
-        var productItem = document.createElement("li");
-        var productBtn = document.createElement("button");
-        productItem.innerHTML = `${product.name}. Ingredients: ${product.ingredients}`;
-        productBtn.innerHTML = "Add to cart";
-        productBtn.id = product.id;
-
-        productBtn.addEventListener("click", () => addProductToCart(product));
-        app.appendChild(productItem).appendChild(productBtn);
+        const productCard = getProductCard(product);
     });
 }
 
-function addProductToCart(product) {
+function getProductCard(product) {
+    const productArticle = document.createElement("article");
+    const productIngredients = listProductIngredients(product.ingredients);
+    const productControls = getProductCardControls(product);
+    productArticle.innerHTML = `
+        <h2>${product.name}</h2>
+        <p>Ingredients:</p>
+    `;
+    app.appendChild(productArticle).appendChild(productIngredients);
+    app.appendChild(productControls);
+}
+
+function listProductIngredients(productIngredients) {
+    const productIngredientsList = document.createElement("ul");
+    productIngredients.forEach((ingredient) => {
+        const productIngredientListItem = document.createElement("li");
+        productIngredientListItem.innerText = `${ingredient}`;
+        productIngredientsList.appendChild(productIngredientListItem);
+    });
+    return productIngredientsList;
+}
+
+function getProductCardControls(product) {
+    const productButton = document.createElement("button");
+    productButton.innerText = "Add to cart";
+    productButton.addEventListener("click", () => addProductToCart(product));
+    return productButton;
+}
+
+function addProductToCart(product, quantity) {
     const existingProduct = searchProductInCart(product.name);
     if (existingProduct) {
-        existingProduct.quantity += 1;
+        existingProduct.quantity++;
         console.log(cart);
     } else {
         cart.push({
@@ -32,6 +54,35 @@ function addProductToCart(product) {
 
 function searchProductInCart(productName) {
     return cart.find((product) => product.name === productName);
+}
+
+const decreaseProductQuantityButton =
+    document.getElementById("decrease-quantity");
+
+decreaseProductQuantityButton.addEventListener("click", () =>
+    updateProductQuantityInput("decrease")
+);
+
+const increaseProductQuantityButton =
+    document.getElementById("increase-quantity");
+
+increaseProductQuantityButton.addEventListener("click", () =>
+    updateProductQuantityInput("increase")
+);
+
+const quantityInput = document.querySelector("input[name='product-quantity']");
+
+function updateProductQuantityInput(expression) {
+    switch (expression) {
+        case "decrease":
+            if (quantityInput.value > 1) quantityInput.value--;
+            break;
+        case "increase":
+            if (quantityInput.value < 9) quantityInput.value++;
+            break;
+        default:
+            console.log("Error has occured, please try again.");
+    }
 }
 
 displayProducts(data);
